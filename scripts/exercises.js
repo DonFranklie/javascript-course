249adf1900791ea55659495b1e3ef993780dba18
DonFranklie <donfranklie@gmail.com>
2024-02-01 22:28:25 +0300

    clicked buton

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -4,8 +4,18 @@ const techButton = document.querySelector('.tech-button');
 
 function toggle(button){
   if(!button.classList.contains("clicked-button")){
+
+    turnOffPreviousButton();
+
     button.classList.add("clicked-button");
   }else if(button.classList.contains("clicked-button")){
     button.classList.remove("clicked-button");
+  }
+}
+
+function turnOffPreviousButton() {
+  const previousButton = document.querySelector('.clicked-button');
+  if(previousButton) {
+    previousButton.classList.remove('clicked-button');
   }
 }
