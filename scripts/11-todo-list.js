2c4edfca00ce7c657ebbcc0fcda7d9031613e467
DonFranklie <donfranklie@gmail.com>
2024-02-14 21:31:35 +0300

    to do list vs html render

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,18 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash car'];
+
+let todoListHtml = '';
+
+for (let i = 0; i < todoList.length; i++){
+ const toDo = todoList[i];
+ const htmlCode = `<p>${toDo}</p>`
+ todoListHtml += htmlCode;   
+  
+}
+
+console.log(todoListHtml)
+
+document.querySelector('.js-todo-list')
+.innerHTML = todoListHtml 
 
 function addTodo(){
   let inputElement = document.querySelector('.js-name-input');
