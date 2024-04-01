d47fece332811ac4d52ffc7584eb6318a8eeeddb
DonFranklie <donfranklie@gmail.com>
2024-04-01 07:53:39 +0300

    render todoList

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,27 +1,34 @@
-const todoList = ['make dinner', 'wash car'];
+const todoList = ['make dinner', 'wash car', 'code js'];
 
-let todoListHtml = '';
+renderTodoList();
 
-for (let i = 0; i < todoList.length; i++){
- const toDo = todoList[i];
- const htmlCode = `<p>${toDo}</p>`
- todoListHtml += htmlCode;   
-  
+function renderTodoList() {
+  let todoListHtml = '';
+
+  for (let i = 0; i < todoList.length; i++){
+  const toDo = todoList[i];
+  const htmlCode = `<p>${toDo}</p>`
+  todoListHtml += htmlCode;   
+    
+  }
+
+  console.log(todoListHtml)
+
+  document.querySelector('.js-todo-list')
+  .innerHTML = todoListHtml 
 }
 
-console.log(todoListHtml)
-
-document.querySelector('.js-todo-list')
-.innerHTML = todoListHtml 
-
 function addTodo(){
-  let inputElement = document.querySelector('.js-name-input');
-  const todoName = inputElement.value.trim();
+    let inputElement = document.querySelector('.js-name-input');
+    const todoName = inputElement.value.trim();
 
 
-  todoList.push(todoName);
+    todoList.push(todoName);
 
-  console.log(todoList);
 
-  inputElement.value = "";
+    console.log(todoList);
+
+    inputElement.value = "";
+
+    renderTodoList();
 }
