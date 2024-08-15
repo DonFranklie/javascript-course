92aacaafac5aa4a361b4317f8b0faec4688ed0c6
DonFranklie <donfranklie@gmail.com>
2024-08-15 06:45:54 +0300

    Lesson 12 part 1

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -1,162 +1,51 @@
-/*
-const stringArray = ['hello', 'welcome', 'search', 'hello'];
+ /*
+const add = function() {
+  console.log(2+3);
+};
 
-let word = 'search';
-let index = -1
-for (let i = 0; i < stringArray.length; i++) {
-  const arrayItem = stringArray[i];
-  if (arrayItem === word){
-    index = i;
-  }
-  
+
+
+function runTwice(fun) {
+  fun();
+  fun();
 }
 
-console.log(index);
-
-
-
-const array = ['green', 'red', 'blue', 'red'];
-
-function findIndex(array, word) {
-  let index = -1;
-
-  for (let i = 0; i < array.length; i++) {
-    const newWord = array[i];
-
-    if (newWord === word) {
-      index = i;
-      break;
-    }
-    
-  }
-
-  return index;
-}
-
-console.log(findIndex(array, 'yellow'));
-
-
-
-function removeEgg(foods) {
-  const newArray = [];
-  
-  for (let i = 0; i < foods.length; i++) {
-    const food = foods[i];
-    
-    if (!(food === 'egg')) {
-      newArray.push(food);
-    }
-  }
-
-  return newArray;
-}
-
-console.log(removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']));
-
-function removeEgg(foods) {
-  const newArray = [];
-  let appearance = 0;
-  foods.reverse();
-  
-  for (let i = 0; i < foods.length; i++) {
-    const food = foods[i];
-    
-    if (!(food === 'egg')) {
-      newArray.push(food);
-    } else if (food === 'egg') {
-      appearance++;
-      if (appearance > 2) {
-        newArray.push(food);
-      }
-    }
-  }
-
-  return newArray.reverse();
-}
-
-console.log(removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']));
-
-
-function removeEgg(foods) {
-  const newArray = [];
-  let appearance = 0;
-  let slicedFoods = foods.slice();
-  
-  for (let i = 0; i < slicedFoods.length; i++) {
-    const food = slicedFoods[i];
-    
-    if (!(food === 'egg')) {
-      newArray.push(food);
-    } else if (food === 'egg') {
-      appearance++;
-      if (appearance > 2) {
-        newArray.push(food);
-      }
-    }
-  }
-
-  return newArray.reverse();
-}
-
-const foods = ['egg', 'apple', 'egg', 'egg', 'ham']
-console.log(removeEgg(foods));
-console.log(foods);
-
-
-
-for (let i = 1; i <= 20; i++) {
-
-  if (i % 3 === 0 && i % 5) {
-    console.log('FizzBuzz');
-  } else if (i % 3 === 0) {
-    console.log('Fizz');
-  } else if (i % 5 === 0) {
-    console.log('Buzz');
-  } else {
-    console.log(i);
-  }
-  
-}
+runTwice(add);
 
 */
 
+document.title = 'App';
 
-function findIndex(array, word) {
-  let index = -1;
+let intervalId;
+let isDisplayingNotification = true;
 
-  for (let i = 0; i < array.length; i++) {
-    const newWord = array[i];
+let messageNumber = 2;
 
-    if (newWord === word) {
-      index = i;
-      break;
-    }
-    
+showNotification();
+
+function addMessages() {
+  messageNumber++;
+}
+function reduceMessages() { 
+  if (messageNumber > 0){
+  messageNumber--;
+  showNotification();
+  } else if (messageNumber === 0){
+    document.title = 'App';
   }
-
-  return index;
 }
 
-// console.log(findIndex(array, 'yellow'));
-
-function unique(array) {
-  let cleanArray = [];
-
-  for (let i = 0; i < array.length; i++) {
-    const arrayItem = array[i];
-
-    if (findIndex(cleanArray, arrayItem) == -1) {
-      cleanArray.push(arrayItem);
+function showNotification() {
+  intervalId = setInterval(function(){
+    if (document.title === 'App'){
+      if (messageNumber === 0){
+        document.title = 'App';
+    
     } else {
-      continue;
+      document.title = `(${messageNumber}) new messages`;
     }
-    
+  } else {
+    document.title = 'App';
   }
-
-  return cleanArray;
+  }, 1000)
 }
-
-const array = ['green', 'red', 'blue', 'red'];
-
-console.log(unique(array));
-
