dd11c586015dd06dc7b151ec0bc12c64f3af6c73
DonFranklie <donfranklie@gmail.com>
2024-08-15 07:23:06 +0300

    Arrow Functions

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -24,7 +24,7 @@ const todoList = [
   function renderTodoList() {
     let todoListHTML = '';
 
-    todoList.forEach(function(todoObject, index) {
+    todoList.forEach((todoObject, index) => {
       const { name, dueDate } = todoObject;
       // const dueDate = todoObject.dueDate;
 
