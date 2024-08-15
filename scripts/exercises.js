4ea3155752491ddedf62d874dd16f9abcef95776
DonFranklie <donfranklie@gmail.com>
2024-08-15 16:06:46 +0300

    Advanced Functions Part 2

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -1,51 +1,66 @@
- /*
-const add = function() {
-  console.log(2+3);
-};
+/*
+function countPositive(nums) {
+  let howMany = 0;
 
+  nums.forEach(element => {
+    if (element > 0) {
+      howMany++;
+      
+    }
+    
+  });
 
-
-function runTwice(fun) {
-  fun();
-  fun();
+  return howMany;
 }
 
-runTwice(add);
+console.log(countPositive([1,-3,5]));
+
+console.log(countPositive([-2, 3, -5, 7, 10]));
+
+
+
+
+const multiply = (num1, num2) =>  num1 * num2;
+
+console.log(multiply(4, 5));
+
+
+
+const array = [1, 2, 3] 
+
+function addNum(array, num) {
+  let newArray;
+  newArray = array.map(value => {
+    return value + num;
+  });
+
+  return newArray;
+}
+
+console.log(addNum(array, 2));
 
 */
 
-document.title = 'App';
+function removeEggs(foods) {
+  let newArray = [];
+  let num = 0;
 
-let intervalId;
-let isDisplayingNotification = true;
-
-let messageNumber = 2;
-
-showNotification();
-
-function addMessages() {
-  messageNumber++;
-}
-function reduceMessages() { 
-  if (messageNumber > 0){
-  messageNumber--;
-  showNotification();
-  } else if (messageNumber === 0){
-    document.title = 'App';
-  }
-}
-
-function showNotification() {
-  intervalId = setInterval(function(){
-    if (document.title === 'App'){
-      if (messageNumber === 0){
-        document.title = 'App';
+  newArray = foods.filter((value) => {
     
-    } else {
-      document.title = `(${messageNumber}) new messages`;
+    if (value === 'egg'){
+      num++;
     }
-  } else {
-    document.title = 'App';
-  }
-  }, 1000)
+
+
+    if (!(value === 'egg') || num > 2) {
+      return true;
+    }
+    
+  
+  });
+
+  return newArray;
 }
+
+console.log(removeEggs(['egg', 'apple', 'egg', 'egg', 'ham']));
+
